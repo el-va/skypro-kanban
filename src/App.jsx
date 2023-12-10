ff711e5f8ae3662b889aa29201e363b72462ed34
Elena <luna24elena@gmail.com>
2023-12-10 16:56:41 +0700

    fix drafts, dublicates

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './vite.svg'
 import './App.css'
-import Container from './components/Container'
+import Container from './components/Container/Container'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import PopBrowse from './components/PopBrowse/PopBrowse'
@@ -10,33 +7,21 @@ import PopExit from './components/PopExit/PopExit'
 import PopNewcard from './components/PopNewcards/PopNewcard'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
+  <>
 
-<div className="wrapper">
-{/* <!-- pop-up start--> */}
-
+  <div className="wrapper">
   <PopExit />
-
   <PopNewcard />
-
   <PopBrowse />
+  <Header />
+  <Main>
+    <Container />
+  </Main>
+  </div>
 
-{/* <!-- pop-up end--> */}
-
-{/* <!-- <a href="">x</a> --> */}
-
-<Header />
-
-<Main>
-  <Container />
-</Main>
-</div>
-
-<script src="js/script.js"></script>
-</>
+  <script src="js/script.js"></script>
+    </>
   )}
 
 export default App;
