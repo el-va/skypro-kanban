67560287e80530ab23b786b8a23b0777e5609795
Elena <luna24elena@gmail.com>
2023-12-16 01:41:43 +0700

    hw2 with question

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const cardList = [
+    {
+      id: 1,
+      theme: "Web Design",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Без статуса",
+      color: "_orange",
+  },
+  {
+      id: 2,
+      theme: "Research",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Нужно сделать",
+      color: "_green",
+  },
+  {
+      id: 3,
+      theme: "Copywriting",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Без статуса",
+      color: "_purple",
+  },
+    {
+      id: 4,
+      theme: "Copywriting",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Тестирование",
+      color: "_purple",
+  },
+];
+
+export const statusList = [
+    "Без статуса",
+    "Нужно сделать",
+    "В работе",
+    "Тестирование",
+    "Готово",
+  ];
