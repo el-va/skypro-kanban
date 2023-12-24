4e7310edd1b2dd82527426088aa5575aaab74eaa
Elena <luna24elena@gmail.com>
2023-12-25 01:08:35 +0700

    add

--- src/components/PopExit/PopExit.styled.js
+++ src/components/PopExit/PopExit.styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { breakpoints } from "../../lib/breakpoints";
+// import { breakpoints } from "../../lib/breakpoints";
 
 export const PopExitPop = styled.div`
   display: none;
